16de92a88599e9c532b3e5692bdc35c2ec92b68c
swantex <121892854+swantex@users.noreply.github.com>
2023-01-12 19:29:43 +0300

    Create baslik.py

---
+++ baslik.py
@@ -0,0 +1,19 @@
+def title():
+	print("\x1bc")
+	print("\n\033[38;5;52m███╗   ██╗███████╗████████╗███████╗██╗     ██╗██╗  ██╗")
+	print("\033[38;5;88m████╗  ██║██╔════╝╚══██╔══╝██╔════╝██║     ██║╚██╗██╔╝")
+	print("\033[38;5;124m██╔██╗ ██║█████╗     ██║   █████╗  ██║     ██║ ╚███╔╝ ")
+	print("\033[38;5;160m██║╚██╗██║██╔══╝     ██║   ██╔══╝  ██║     ██║ ██╔██╗ ")
+	print("\033[38;5;196m██║ ╚████║███████╗   ██║   ██║     ███████╗██║██╔╝ ██╗")
+	print("\033[38;5;160m╚═╝  ╚═══╝╚══════╝   ╚═╝   ╚═╝     ╚══════╝╚═╝╚═╝  ╚═╝")
+	print("\033[38;5;124m    =========== https://github.com/swantex =========    ")
+
+def user_options():
+	print("\n\033[38;5;7m--------- ayarlar: ---------\n")
+	print("1) Hesapları Kontrol Et")
+	print("2) Birleşik Listedeki Ülkeleri ve İstatistikleri Kontrol Et")
+	print("3) Ülkeleri Listeden Eleyin (hesapları herhangi bir VPN ile kontrol edin)\n")
+
+	
+			
+		
